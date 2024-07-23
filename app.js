c4bdc7f18241da3ba32b9de2fe4d24f2555b7506
marin-duv <marinz3us@gmail.com>
2024-07-23 01:13:50 -0600

    actualizo app.js funcion random

--- app.js
+++ app.js
@@ -1,29 +1,152 @@
-/* ejemplo de numero secreto*/
+
+// ejemplo de numero secreto
+//******************************************************************************************
+
 alert (`Bienvenido al juego del número secreto`);
 //tema comentarios y else
-
 // estas son las variables
-let numeroSecreto = 8;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
-console.log(`Valor de intento: ${numeroUsuario}`);
 
-console.log(numeroUsuario);
+let numeroSecreto = parseInt(Math.floor(Math.random()*10))+1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
+console.log (numeroSecreto);
 
-if (numeroUsuario == numeroSecreto) {
-    //acerto, fue verdadera la condicion
-    // se utilizop templete strings
-    // templete strings `texto: ${variable}`
-    alert(`¡Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    //la condicion no se cumplio 
-    alert('Lo siento, No acertaste el número');
-    console.log(`Èl numero secreto era ${numeroSecreto}`);
+//loop while ; lazo de repeticion:
+while (numeroUsuario != numeroSecreto) {
+    //numeroSecreto = parseInt(numeroSecreto);
+    console.log (typeof(`numero SECRETO ...... ${numeroSecreto}`));
+    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
+    console.log(`Valor de intento: ${numeroUsuario}`);
+    console.log(typeof(numeroUsuario));
+    if (numeroUsuario == numeroSecreto) {
+        //acerto, fue verdadera la condicion
+        // se utilizop templete strings
+        // templete strings `texto: ${variable}`
+        alert(`¡Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    } else {
+        //condicionales anidadas
+        if (numeroUsuario > numeroSecreto) {
+            alert(`El numero secreto es menor`);
+        } else {
+            alert(`El numero secreto es mayor`);
+        }
+        //incrementamos el contador cuando la persona no acierta
+        intentos++;
+        palabraVeces = 'veces';
+        if (intentos > maximosIntentos ){
+            alert (`LLegaste al numero maximo de ${maximosIntentos} intentos`);
+            break;
+        }
+        //la condicion no se cumplio 
+        //alert('Lo siento, No acertaste el número');
+        //console.log(`Èl numero secreto era ${numeroSecreto}`);
+    }
+}
+//******************************************************************************************
+
+
+/*
+//ejemplo while del 1 al 10 
+let numero = 1;
+while (numero <= 10 ) {
+    console.log(`El número es: ${numero}`);
+    numero ++;
 }
 
+*/
+
+/*
+
+//ejemplo while del 10 al 0 
+let numero = 10;
+while (numero >= 0 ) {
+    console.log(`El número es: ${numero}`);
+    numero --;
+}
+
+*/
+/*
+//ejemplo de cuenta regresiva numero gasta el 0
+let numero = prompt(`Indica un número por favor: ` );
+while (numero >= 0 ) {
+    console.log(`El número es: ${numero}`);
+    numero --;
+}
+*/
+
+/*
+//ejemplo de cuenta progresiva numero gasta el 0
+let numeroMaximo = prompt("Indica un número por favor:");
+let contador = 0;
+while (contador <= numeroMaximo ) {
+        console.log(`El número es: ${contador}`);
+        contador ++;
+}
+*/
 
 
 
-/* ejemplo de alert
+/*
+//ejemplo buen fin de semana
+let diaSemana = prompt ("Me indicas el dia actual, por favor:");
+console.log(`dia de la semana :  ${diaSemana}`);
+if ( diaSemana == "sabado" || diaSemana == "domingo") {
+    alert(`¡Buen fin de semana! a`);
+} else {
+    alert(`¡Buena Semana!`);   
+}
+*/
+
+
+
+
+/*
+//ejemplo numero negativo
+let numeroUsuario = prompt ( " Me indicas un número: ");
+console.log(`numeroUsuario: ${numeroUsuario}`);
+
+if (numeroUsuario >0  ){
+    alert (`el numero es positivo`);
+} else if (numeroUsuario < 0 ) {
+    alert (`el numero es negativo`);
+} else {
+    alert (`El numero es CERO`);
+}
+
+*/
+
+
+/*
+//ejemplo de sistema de puntacion 
+
+let puntacion = prompt ("me indicas tu puntacion");
+
+if (puntacion >= 100) {
+    alert (`Felicidases has ganado`);
+} else {
+    alert (`Intentalo nuevamente para ganar`);
+}
+
+*/
+
+/*
+//ejemplo saldo de cuenta bancaria con templete strings
+let saldoActual = 15000;
+alert(`Tu saldo actual es $ ${saldoActual}`);
+*/
+
+/*
+//ejemplo de saludo con nombre 
+let nombre = prompt (`Escribe tu nombre por favor : `);
+alert(`Bienvenido a nuestro curso de JAVASCRIPT: ${nombre}`);
+*/
+
+
+
+/* 
+//ejemplo de alert
 alert ('¡Bienvenida y bienvenido a nuestro sitio web!');
 */
 
